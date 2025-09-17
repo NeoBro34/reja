12b83b32763e2b9ea7ad334f707c6fa71d3c5c5e
Nurislom ( N E O ) <nurislomkhurramov@gmail.com>
2025-09-17 15:02:12 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
+
 console.log("Web Serverni boshlash");
 
 const express = require("express");
-const app = express();          //<-- expresning app objectini yuboradi. Bu object orqali express serverni quramiz.
-const http = require("http");  // 4-stepda. Server qurishta ishlatilinadi. "http" CORE moduldir.
+const app = express();
+const res = require("express/lib/response");
+const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("EROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+ 
 
-// 1-step. Kirish code. Expressga kirib kelayotgan ma'lumotlarga bog'liq kodlar yoziladi.
+// 1-step. Kirish code.
 
 app.use(express.static("public"));    
-/* ^-- bu har qanday brauserdan kirib kelayotgan so'rovlar uchun "public" folderi ochiq degani.
-Faqatgina "public" folderini ko'ra oladi degani. EX: gogle chrom expres serverimizga request qilayotgan vaqti "public" folderini clientlarga ochib beryabmiz degani. "public" folderning ichiga CSSni kerak bo'ladigan IMGlarni shuning ichiga joylashtiramiz.
-*/
 app.use(express.json());
-/* ^-- Client hamda web serverlar orasidagi datan "json" formatda. Shunday ekan kirib kelyotgan "json" formatdagi datani object holatiga o'girib beradi
-*/
 app.use(express.urlencoded({extended: true}));
-/* ^-- HTMLda traditional FORM request degan shakl bor bu traditional request qilish insturmenti yani FORMda biror narsani post qilsak bizning "express" serverimiz qabul qilib oladi. Bu kodni yozmasak HTML FORMda post qilingan narsalarni express qabul qilmaydi.Ignore qiladi serverga kiritmaydi
-*/
 
 
 // 2-step. Session code.
 
 
-// 3-step. Views code. Beckendda Frontend ya'ni HTMLni yasaymiz hamda clientga yuboramiz.Viewni yasash uchun "ejs"@3.1.6 packageni o'rnatib olamiz. 
-app.set("views","views");       // Folderni ko'rsatyabmiz hamda yangi Folder ochib olamiz.2-dagi "views" folder nomi
-app.set("view engine", "ejs"); //  Bu yerda "view engine"ni "ejs" ekanligini ko'rsatyabmiz.
-
+// 3-step. Views code.
+app.set("views","views"); 
+app.set("view engine", "ejs");
 
 // 4-step. Routing code. Serverni yaratish.
-app.get("/hello", function(req, res) {
-    res.send("<h1>Hello World</h1>");
+app.post("/create-item", function(req, res) {
+    
 });
-app.get("/gift", function(req, res) {
-    res.send("<h1>Siz sovg'alar bo'limidasiz</h1>");
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+});
+
+app.get("/", function(req, res) {
+    res.render("harid" );
 });
 
 const server = http.createServer(app);
