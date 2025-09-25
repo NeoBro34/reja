7261f84f35b767646e07c1db9b722bdcbc03decc
Nurislom ( N E O ) <nurislomkhurramov@gmail.com>
2025-09-25 17:09:28 +0900

    fix : ERROR on connection MongoDB fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const mongodb = require("mongodb")
 
 let db;
-const connectionString = "mongodb+srv://NeoBro_db_user:jiouYnrsf6CIxc3T@cluster0.yqv7fdb.mongodb.net/";
+const connectionString = "mongodb+srv://NeoBro_db_user:jiouYnrsf6CIxc3T@cluster0.yqv7fdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongodb.connect(
     connectionString, {
