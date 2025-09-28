08e156dae45357e85df573e94460558edd35c692
Nurislom ( N E O ) <nurislomkhurramov@gmail.com>
2025-09-29 00:02:13 +0900

    feat: MITASK C

--- train.js
+++ train.js
@@ -1,3 +1,91 @@
+
+/*
+TASK-C
+
+Shop nomli class tuzing, va bu class 3 xill parametr qabul qilsin.
+Hamda classning quyidagdek 3'ta metodi bo'lsin:
+
+1) qoldiq
+2) sotish
+3) qabul
+
+Har bir metod ishga tushgan vaqtda log qilinsin
+
+MASALAN:
+const shop = new Shop(4, 5, 2)
+
+shop.qoldiq();
+natija qaytishi kerak: Hozir 20: 40'da 4'ta non, 5'ta lag'mon va 2'ta cola mavjud
+
+shop.sotish("non", 3); 
+shop.qabul("cola", 4);
+shop.qoldiq();
+Natija qaytishi kerak: Hozir 20:50da 1ta non, 5ta lag'mon va 6ta cola mavjud!
+*/
+const moment = require('moment');
+const time = moment().format("HH:mm");
+
+
+class Shop {
+    constructor(a, b, c) {
+        this.non = a;
+        this.lagman = b;
+        this.cola = c;
+    }
+    qoldiq() {
+        console.log(`Hozir ${time}da ${this.non}ta non, ${this.lagman}ta lag'mon va ${this.cola}ta cola mavjud`);
+    }
+    sotish(mahsulot, miqdori) {
+        if (mahsulot === "non") {
+            return this.non -= miqdori;
+        }
+        if (mahsulot === "lag'mon") {
+            return this.lagman -= miqdori;
+        }
+        if (mahsulot === "cola") {
+            return this.cola -= miqdori;
+        } else {
+            console.log("Iltimos mahsulot nomi yoki miqdorini to'g'ri kiriting!")
+        }
+    } 
+
+    qabul(mahsulot, miqdori) {
+       if (mahsulot === "non") {
+            return this.non += miqdori;
+        }
+        if (mahsulot === "lag'mon") {
+            return this.lagman += miqdori;
+        }
+        if (mahsulot === "cola") {
+            return this.cola += miqdori;
+        } else {
+            console.log("Iltimos mahsulot nomi yoki miqdorini to'g'ri kiriting!")
+        }
+    }
+};
+console.log("Bor edi");
+const shop = new Shop(4,5,2);
+shop.qoldiq();
+
+console.log("Sotilgandan so'ngi hisob");
+
+shop.sotish("non",3);
+shop.sotish("cola",2)
+shop.qoldiq();
+
+console.log("Tovar kirib kelgandan so'ngi hisob");
+
+shop.qabul("non",15);
+shop.qabul("cola",10);
+shop.qabul("lag'mon",2)
+shop.qoldiq();
+
+
+
+
+
+
+
 // TASK B
 
 // Shunday, function tuzingki, bu function yagona parametrga ega bo'lib
@@ -6,16 +94,16 @@
 // Masalan: countDigits("ad2a54y79wet0sfgb9")
 // Yuqoridagi string tarkibida 7 dona raqam qatnashganligi uchun, natija 7 qaytadi
 
-function sonlarniHisoblaymiz(a) {
-    let hisob = 0;
-    for (const ele of a) {
-        if(ele >= "0" && ele <= "9") {       // Loop qil "a"ni ichidagi "ele" kotta bo'lasa 0 va kichik bo'lsa 9 dan +1dan hisobla 
-            hisob++;
-        }
-    }
-    return hisob;
-}
-console.log(sonlarniHisoblaymiz("ad2a54y79wet0sfgb9"));
+// function sonlarniHisoblaymiz(a) {
+//     let hisob = 0;
+//     for (const ele of a) {
+//         if(ele >= "0" && ele <= "9") {       // Loop qil "a"ni ichidagi "ele" kotta bo'lasa 0 va kichik bo'lsa 9 dan +1dan hisobla 
+//             hisob++;
+//         }
+//     }
+//     return hisob;
+// }
+// console.log(sonlarniHisoblaymiz("ad2a54y79wet0sfgb9"));
 
 
 
